ddc7b40b77f8e84fce49e95022311f6588e23468
won <yangwon717@gmail.com>
2022-05-28 18:40:19 +0900

    main

--- book.py
+++ book.py
@@ -1,28 +1,21 @@
-while True
-    print("""도서 관리 시스템에 오신 걸 환영합니다!
-    원하시는 메뉴를 고르시오.
+import os
 
-1. 도서 조회
-2. 도서 대여
-3. 도서 반납
-4. 도서 기증
-5. 나의 정보
-6. 나가기
-""")
+def SelectMenu():
+    os.system('cls')
+    print("            *도서관리 프로그램*")
+    print("┏━━━━━━━━━━━━━━━━추천 도서━━━━━━━━━━━━━━━━━┓")
+    print("┃ 1.천사의 부름 2.미움 받을용기 3. 긴긴밤  ┃")
+    print("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+    print("1:도서 조회")
+    print("2:도서 대여")
+    print("3:도서 반납")
+    print("4:도서 기증")
+    print("5:나의 정보")
+    print("0:종료")
+    user_input = input("메뉴 선택 ☞: ")
+    if user_input == '0':
+        return
 
-    menu = input()
-
-    if menu == "1":
-        manager.showbooks()
-    elif menu == "2":
-        title = input("제목을 입력하세요 : ")
-        author = input("저자를 입력하세요 : ")
-        manager.appendbook(book(title, author))
-    elif menu == "3":
-        pass
-    elif menu == "4":
-        pass
-    elif menu == "5":
-        pass
-    else:
-        print("올바른 번호를 입력하세요!!")
+while 1:
+    SelectMenu()
+    break
